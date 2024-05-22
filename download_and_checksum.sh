c66aa644dc318b55637314227771fd84c7978bcc
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2024-05-22 18:04:12 +0300

    Update download_and_checksum.sh

--- download_and_checksum.sh
+++ download_and_checksum.sh
@@ -10,9 +10,9 @@ curl -L -O "https://github.com/realm/realm-swift/releases/download/v$REALM_VERSI
 
 REALM_CHECKSUM=$(swift package compute-checksum Realm.xcframework.zip)
 
-curl -L -O "https://github.com/realm/realm-swift/releases/download/v$REALM_VERSION/RealmSwift@$XCODE_VERSION.xcframework.zip"
+curl -L -O "https://github.com/realm/realm-swift/releases/download/v$REALM_VERSION/RealmSwift@$XCODE_VERSION.spm.zip"
 
-REALM_SWIFT_CHECKSUM=$(swift package compute-checksum RealmSwift@$XCODE_VERSION.xcframework.zip)
+REALM_SWIFT_CHECKSUM=$(swift package compute-checksum RealmSwift@$XCODE_VERSION.spm.zip)
 
 echo "REALM_CHECKSUM=$REALM_CHECKSUM" >> $GITHUB_ENV
 echo "REALM_SWIFT_CHECKSUM=$REALM_SWIFT_CHECKSUM" >> $GITHUB_ENV
